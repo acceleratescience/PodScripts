0f97f75490d8bfa12c2c33fcdea4a548a73006cd
Radzim <57621493+Radzim@users.noreply.github.com>
2025-11-06 18:06:24 +0000

    Update start_cli.sh

--- start_cli.sh
+++ start_cli.sh
@@ -3,10 +3,11 @@ set -e
 
 apt-get update -y
 apt-get install -y --no-install-recommends wget ca-certificates
+rm -rf /var/lib/apt/lists/*
 
-wget https://github.com/tsl0922/ttyd/releases/latest/download/ttyd.x86_64 -O /usr/local/bin/ttyd
+wget https://github.com/tsl0922/ttyd/releases/download/1.7.3/ttyd.x86_64 -O /usr/local/bin/ttyd
 chmod +x /usr/local/bin/ttyd
 
-nohup ttyd -p 8880 bash -i >/tmp/cli.log 2>&1 &
+nohup /usr/local/bin/ttyd --writable -p 8880 bash -i >/tmp/cli.log 2>&1 &
 
 echo "Command-line interface started on port 8880. Logs: /tmp/cli.log"
