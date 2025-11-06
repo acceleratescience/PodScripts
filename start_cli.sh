e75edd2f162d242181b4cd87852ab900d1844584
Radzim <57621493+Radzim@users.noreply.github.com>
2025-11-06 17:29:47 +0000

    Update start_cli.sh

--- start_cli.sh
+++ start_cli.sh
@@ -1,7 +1,10 @@
+#!/bin/bash
+set -e
+
 apt-get update -y
 apt-get install -y --no-install-recommends wget ca-certificates
 
-wget https://github.com/tsl0922/ttyd/releases/latest/download/ttyd.x86_64-musl -O /usr/local/bin/ttyd
+wget https://github.com/tsl0922/ttyd/releases/latest/download/ttyd.x86_64 -O /usr/local/bin/ttyd
 chmod +x /usr/local/bin/ttyd
 
 nohup ttyd --check-origin=false -p 8880 bash -i >/tmp/cli.log 2>&1 &
